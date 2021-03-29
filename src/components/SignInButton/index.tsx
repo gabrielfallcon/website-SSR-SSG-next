3222c99bf376cd77ad2e1b48eeb3cdb71392dec6
Gabriel da Silva Nascimento <gabriel.nascimento@compasso.com.br>
2021-03-28 21:25:59 -0300

    first-commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,30 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#04d361" size={20} />
+
+      Gabriel Nascimento
+
+      <FiX color="#919180" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#eba417" size={20} />
+
+      Sign in with Github
+    </button>
+  )
+}
