e866a077a3aaafaf343f5fe5138d88aaee483653
Gabriel da Silva Nascimento <gabriel.nascimento@compasso.com.br>
2021-03-30 22:21:36 -0300

    connected in faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession} from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" size={20} />
 
-      Gabriel Nascimento
+      {session.user.name}
 
       <FiX color="#919180" className={styles.closeIcon} />
     </button>
@@ -21,6 +23,7 @@ export function SignInButton() {
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" size={20} />
 
